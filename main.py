037c2cec828221dfcd407aa94eb09d9ba9bae7f0
RheingoldRiver <18037011+RheingoldRiver@users.noreply.github.com>
2024-11-26 15:12:20 -0600

    example of how NOT to do optional arguments

--- main.py
+++ main.py
@@ -7,13 +7,20 @@ reader = csv.DictReader(f)
 WIKITEXT = """{{{{Clothing infobox
 |uid={}
 |display={}
+|craft slots={}
 }}}}"""
 
 # Example of reusing the reader
 for row in reader:
+
+    craft_slot_value1 = '{{CraftSlot|' + row['Craft Slot 1 ID'] + '|' + row['Craft Slot 1 Amount'] + '}}'
+    craft_slot_value2 = '{{CraftSlot|' + row['Craft Slot 2 ID'] + '|' + row['Craft Slot 2 Amount'] + '}}'
+    craft_slot_value3 = '{{CraftSlot|' + row['Craft Slot 3 ID'] + '|' + row['Craft Slot 3 Amount'] + '}}'
+
     print(WIKITEXT.format(
         row['UID'],
-        row['-Friendly Name-']
+        row['-Friendly Name-'],
+        craft_slot_value1 + '\n' + craft_slot_value2 + '\n' + craft_slot_value3
     ))
 
 # cleanup
