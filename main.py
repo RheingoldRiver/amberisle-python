1cccad61216e485fddeb82fe91dc74fc309bf06f
RheingoldRiver <18037011+RheingoldRiver@users.noreply.github.com>
2024-11-26 15:06:28 -0600

    printing each template

--- main.py
+++ main.py
@@ -4,9 +4,17 @@ f = open('data.csv', mode='r', newline='')
 # Load the CSV data
 reader = csv.DictReader(f)
 
+WIKITEXT = """{{{{Clothing infobox
+|uid={}
+|display={}
+}}}}"""
+
 # Example of reusing the reader
 for row in reader:
-    print(row)
+    print(WIKITEXT.format(
+        row['UID'],
+        row['-Friendly Name-']
+    ))
 
 # cleanup
 f.close()
